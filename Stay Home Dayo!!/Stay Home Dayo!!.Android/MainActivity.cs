da0d4a67a53e999e4aa55a2db7581b502bc7dfdd
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2020-09-14 16:37:01 +0800

    Initial Release (Pre-Alpha)

---
+++ Stay Home Dayo!!/Stay Home Dayo!!.Android/MainActivity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+using Xamarin.Forms;
+using Stay_Home_Dayo__.Droid;
+using Java.Security;
+
+[assembly: Dependency(typeof(AudioRender))]
+
+namespace Stay_Home_Dayo__.Droid
+{
+    [Activity(Label = "Stay_Home_Dayo__", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    public class AudioRender : IAudioService
+    {
+        public void PlayAudioFile(string fileName)
+        {
+            var player = new MediaPlayer();
+            var file = global::Android.App.Application.Context.Assets.OpenFd(fileName);
+            player.SetDataSource(file.FileDescriptor, file.StartOffset, file.Length);
+            player.Prepared += (s, e) => { player.Start(); };
+            player.Prepare();
+  
+        }
+
+
+
+
+        /*
+         public void PlayAudioFile(string fileName)
+        {
+            var player = new MediaPlayer();
+            var file = global::Android.App.Application.Context.Assets.OpenFd(fileName);
+            player.SetDataSource(file.FileDescriptor, file.StartOffset, file.Length);
+            player.Prepared += (s, e) => { player.Start(); };
+            player.Prepare();
+
+        }
+        */
+    }
+
+}
---
+++ Stay Home Dayo!!/Stay Home Dayo!!/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
+
+using System.IO;
+using System.Reflection;
+
+public interface IAudioService
+{
+    void PlayAudioFile(string filename);
+}
+
+namespace Stay_Home_Dayo__
+{
+
+
+    public partial class MainPage : ContentPage
+    {
+        int lblValue = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_clicked(object sender, EventArgs e)
+        { 
+            DependencyService.Get<IAudioService>().PlayAudioFile("stayhome.mp3");
+            lblValue++;
+            lblCount.Text = lblValue.ToString();
+
+        }
+    }
+}
+
